145a86744810ed6e4ebe4f56525538eaba1916f2
javobal <javier.balam@gmail.com>
2024-11-05 18:09:52 -0600

    test env vars

--- aws-chat-app/src/app/page.tsx
+++ aws-chat-app/src/app/page.tsx
@@ -19,6 +19,8 @@ export default function Home() {
         <SignIn />
 
         <UserAvatar />
+
+        <h3>{process.env.AUTH_COGNITO_ID}</h3>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
